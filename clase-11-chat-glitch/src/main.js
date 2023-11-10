4001a7df5ba73a743cc72c9094fdd59415b1140d
mariano aquino <mariano.aquino@gmail.com>
2023-11-09 21:05:14 -0300

    agrega material clase 11 - chat

---
+++ clase-11-chat-glitch/src/main.js
@@ -0,0 +1,29 @@
+import express from 'express'
+import { PORT } from './config.js'
+import { Server } from 'socket.io'
+import { mensajeManager } from './services/MensajeManager.js'
+
+const app = express()
+
+const server = app.listen(PORT, () => { console.log(`escuchando en puerto ${PORT}`) })
+
+const io = new Server(server)
+
+app.use('/static', express.static('./static'))
+app.use(express.static('./views'))
+
+io.on('connection', async (socket) => {
+
+  socket.emit('mensajes', await mensajeManager.obtenerTodos())
+
+  socket.broadcast.emit('nuevoUsuario',
+    socket.handshake.auth.username
+  )
+
+  socket.on('mensaje', async msg => {
+    console.log(msg)
+    await mensajeManager.agregar(msg)
+    io.sockets.emit('mensajes', await mensajeManager.obtenerTodos())
+  })
+
+})
---
+++ clase-11-chat-glitch/static/js/index.js
@@ -0,0 +1,61 @@
+const form = document.querySelector('form')
+const input = document.querySelector('input')
+const ulMensajes = document.querySelector('ul')
+
+let username
+
+Swal.fire({
+  title: 'CoderChat',
+  input: 'text',
+  text: 'Ingrese su nombre:',
+  inputValidator: value => {
+    return !value && 'debe ingresar un nombre!'
+  },
+  allowOutsideClick: false,
+}).then(result => {
+  username = result.value
+  document.querySelector('input')?.focus()
+  empezarAChatear()
+})
+
+function empezarAChatear() {
+  const socket = io({
+    auth: {
+      username
+    }
+  })
+
+  form?.addEventListener('submit', event => {
+    event.preventDefault()
+    if (input) {
+      enviarMensaje(username, input.value)
+      input.value = ''
+    }
+  })
+
+  function enviarMensaje(username, text) {
+    socket.emit('mensaje', {
+      username,
+      text
+    })
+  }
+
+  socket.on('mensajes', msgs => {
+    if (ulMensajes) {
+      ulMensajes.innerHTML = ''
+      for (const { username, text } of msgs) {
+        const li = document.createElement('li')
+        li.innerHTML = `${username}: ${text}`
+        ulMensajes?.appendChild(li)
+      }
+    }
+  })
+
+  socket.on('nuevoUsuario', nombreUsuario => {
+    Swal.fire({
+      text: `${nombreUsuario} está en linea`,
+      toast: 'true',
+      position: 'top-right'
+    })
+  })
+}
